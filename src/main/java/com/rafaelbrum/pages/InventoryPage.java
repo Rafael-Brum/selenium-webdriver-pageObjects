d25ac9a3abad4a5a34e48b96ed3d3f87591e44db
Rafael Brum <rafaelmoa.brum@gmail.com>
2026-05-03 22:33:31 -0300

    fix: stabilize inventory add-to-cart click in headless runs (refs #15)

--- src/main/java/com/rafaelbrum/pages/InventoryPage.java
+++ src/main/java/com/rafaelbrum/pages/InventoryPage.java
@@ -1,7 +1,10 @@
 package com.rafaelbrum.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 
 public class InventoryPage extends BasePage {
     private final By inventoryTitle = By.cssSelector("span[data-test='title']");
@@ -17,7 +20,14 @@ public class InventoryPage extends BasePage {
     }
 
     public InventoryPage addProductToCartById(String productId) {
-        click(By.id("add-to-cart-" + productId));
+        By addButton = By.id("add-to-cart-" + productId);
+        WebElement element = wait.visible(addButton);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block:'center'});", element);
+        try {
+            element.click();
+        } catch (WebDriverException ex) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
         return this;
     }
 
